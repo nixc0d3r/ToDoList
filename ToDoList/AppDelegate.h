c819a041a7e66adce0b459322e2f944e8048ba2f
ANTON MELNIKOV <nixc0d3r@MacBook-Air-Nixc0d3r.local>
2015-01-21 20:14:17 +0300

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by ANTON MELNIKOV on 21.01.15.
+//  Copyright (c) 2015 ANTON MELNIKOV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
